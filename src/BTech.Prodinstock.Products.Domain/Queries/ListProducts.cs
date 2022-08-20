0b6687a3aab5261446797b3d0d67e29d328dac57
Kevin B <kevin@blenner.tech>
2022-08-20 22:41:44 +0200

    feat: list products

---
+++ src/BTech.Prodinstock.Products.Domain/Queries/ListProducts.cs
@@ -0,0 +1,42 @@
+﻿using BTech.Prodinstock.Products.Domain.Entities;
+
+namespace BTech.Prodinstock.Products.Domain.Queries
+{
+    public sealed class ListProducts
+    {
+        private readonly IReadRepository<Product> _readRepository;
+
+        public ListProducts(IReadRepository<Product> readRepository)
+        {
+            _readRepository = readRepository;
+        }
+
+        public async ValueTask<IEnumerable<ExistingProduct>> ExecuteAsync()
+        {
+            return (await _readRepository.ListAsync())
+                .Select(c => new ExistingProduct
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    BuyingPrice = c.BuyingPrice,
+                    VATRate = c.VATRate,
+                    CreationDate = c.CreationDate,
+                    Description = c.Description,
+                    NumberInStock = c.NumberInStock,
+                    SalePrice = c.SalePrice
+                });
+        }
+    }
+
+    public sealed class ExistingProduct
+    {
+        public string Id { get; set; } = null!;
+        public DateTime CreationDate { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public short NumberInStock { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal VATRate { get; set; }
+        public decimal BuyingPrice { get; set; }
+    }
+}
